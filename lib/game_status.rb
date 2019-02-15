5275013ae4b570d790849f4042fd3963c998d128
Samantha Wong <smiwong920@gmail.com>
2019-02-15 22:25:18 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,6 +14,10 @@ WIN_COMBINATIONS = [
   [2,4,6]
   ]
 
-def won(board)
-  
-end 
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
